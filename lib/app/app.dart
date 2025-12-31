6302a87f50b819ceb08a71668027b4fb17912a04
YasmineRaef <yasmineraef76@gmail.com>
2025-12-31 17:28:24 -0500

    Initializing Flutter Project 'Tetrix'

---
+++ lib/app/app.dart
@@ -0,0 +1,20 @@
+import "package:flutter/material.dart";
+
+import "../presentation/home_page.dart";
+
+class Tetrix extends StatelessWidget {
+  const Tetrix({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
